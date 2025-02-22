8101c968838206ee1473922b7f34f5241c82413a
Prajakta Sake <ajay.sake.1992@gmail.com>
2025-02-22 20:45:30 +0530

    added first layout page home

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import CompanyDropdown from "./components/CompanyDropdown";
 import axios, { type AxiosError } from "axios";
+import Home from "./pages/Home";
 // import GuaranteeCards from "./components/GuaranteeCards";
 // import LanguageSwitcher from "./components/LanguageSwitcher";
 // import GuaranteeDetails from "./components/GuaranteeDetails";
@@ -14,38 +15,10 @@ export type Company = {
 };
 
 function App() {
-  const [companyDropdowns, setCompanyDropdowns] = useState<Company[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | "">();
-
-  useEffect(() => {
-    const fetchCompanyDropdown = async () => {
-      try {
-        const response = await axios.get(
-          "http://localhost:5000/companyDropdown"
-        );
-        setCompanyDropdowns(response.data);
-      } catch (error) {
-        setError((error as AxiosError).message);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchCompanyDropdown();
-  }, []);
-
-  const handleChange = (selectedCompany: string) => {
-    console.log("selectedCompany", selectedCompany);
-  };
-
   return (
     <>
       {/* <LanguageSwitcher /> */}
-      <CompanyDropdown
-        companyDropdowns={companyDropdowns}
-        error={error}
-        onChange={handleChange}
-      />
+      <Home />
     </>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,68 @@
-import React from "react";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useEffect, useState } from "react";
+import CompanyDropdown from "../components/CompanyDropdown";
+import axios, { type AxiosError } from "axios";
+import type { Company } from "../App";
+import GuaranteeCards from "../components/GuaranteeCards";
+import SearchForm from "../components/SearchForm";
+import GuaranteeTable from "../components/GuaranteeTable";
+import Footer from "../components/Footer";
 
-const Home = () => {
-  return <div>Home</div>;
+const Home: React.FC = () => {
+  const [companyDropdowns, setCompanyDropdowns] = useState<Company[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | "">();
+
+  useEffect(() => {
+    const fetchCompanyDropdown = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:5000/companyDropdown"
+        );
+        setCompanyDropdowns(response.data);
+      } catch (error) {
+        setError((error as AxiosError).message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCompanyDropdown();
+  }, []);
+
+  const handleChange = (selectedCompany: string) => {
+    console.log("selectedCompany", selectedCompany);
+  };
+
+  return (
+    <div className="container-fluid">
+      {/* Company Selection */}
+      <div className="container mt-3">
+        <CompanyDropdown
+          companyDropdowns={companyDropdowns}
+          error={error}
+          onChange={handleChange}
+        />
+      </div>
+
+      {/* Guarantee Cards */}
+      <div className="container mt-4">
+        <GuaranteeCards />
+      </div>
+
+      {/* Search Form */}
+      <div className="container mt-4">
+        <SearchForm />
+      </div>
+
+      {/* Guarantee Table */}
+      <div className="container mt-4">
+        <GuaranteeTable />
+      </div>
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
