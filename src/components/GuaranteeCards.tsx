0cc805dd1680c4c5e5675a1f68af5de57cec75f4
Prajakta Sake <ajay.sake.1992@gmail.com>
2025-03-13 23:09:33 +0530

    code refactroing

--- src/components/GuaranteeCards.tsx
+++ src/components/GuaranteeCards.tsx
@@ -5,62 +5,23 @@ import Card, { type CardDetails } from "./Card";
 import axios, { type AxiosError } from "axios";
 import { useTranslation } from "react-i18next";
 
-const GuaranteeCards = () => {
-  const [guaranteeData, setGuaranteeData] = useState<CardDetails[]>([]);
+type GuaranteeCardsProps = {
+  guaranteeData: CardDetails[];
+  clickCard: (index1: number, index2: number) => void;
+};
+
+const GuaranteeCards = ({ guaranteeData, clickCard }: GuaranteeCardsProps) => {
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | "">();
   const [showMoreCards, setShowMoreCards] = useState(false);
   const { t } = useTranslation();
-
-  useEffect(() => {
-    const fetchLandingPageData = async () => {
-      try {
-        const response = await axios.get("http://localhost:5000/cardRecords");
-        setGuaranteeData(response.data);
-      } catch (error) {
-        console.log("failed to fetch data");
-        setError((error as AxiosError).message);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchLandingPageData();
-  }, []);
-
-  // Function to swap two users based on index
-  const handleClick = (index1: number, index2: number) => {
-    setGuaranteeData((prevGuarantees) => {
-      if (
-        index1 < 0 ||
-        index2 < 0 ||
-        index1 >= prevGuarantees.length ||
-        index2 >= prevGuarantees.length
-      ) {
-        return prevGuarantees; // Return unchanged if out of bounds
-      }
-
-      // Create a shallow copy of the array
-      const updatedGuarantees = [...prevGuarantees];
-
-      // Swap users at index1 and index2
-      [updatedGuarantees[index1], updatedGuarantees[index2]] = [
-        updatedGuarantees[index2],
-        updatedGuarantees[index1],
-      ];
-
-      updatedGuarantees[index1].status = "Active";
-      updatedGuarantees[index2].status = "Pending";
-
-      return updatedGuarantees;
-    });
-  };
 
   return (
     <>
       <div className="d-flex flex-column flex-md-row gap-3 justify-content-between">
         {guaranteeData.slice(0, 2).map((item, index) => (
           <div key={item.id} className="col-12 col-md-5">
-            <Card item={item} onClick={() => handleClick(0, index)} />
+            <Card item={item} onClick={() => clickCard(0, index)} />
           </div>
         ))}
 
@@ -83,7 +44,7 @@ const GuaranteeCards = () => {
               key={item.id}
               className={`col-12 col-md-4 ${styles.cardWrapper}`}
             >
-              <Card item={item} onClick={() => handleClick(0, index + 2)} />
+              <Card item={item} onClick={() => clickCard(0, index + 2)} />
             </div>
           ))}
         </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,9 +7,11 @@ import GuaranteeCards from "../components/GuaranteeCards";
 import SearchForm from "../components/SearchForm";
 import GuaranteeTable from "../components/GuaranteeTable";
 import Footer from "../components/Footer";
+import type { CardDetails } from "../components/Card";
 
 const Home: React.FC = () => {
   const [companyDropdowns, setCompanyDropdowns] = useState<Company[]>([]);
+  const [guaranteeData, setGuaranteeData] = useState<CardDetails[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | "">();
 
@@ -17,7 +19,7 @@ const Home: React.FC = () => {
     const fetchCompanyDropdown = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:5000/companyDropdown"
+          "http://localhost:3000/companyDropdown"
         );
         setCompanyDropdowns(response.data);
       } catch (error) {
@@ -29,8 +31,50 @@ const Home: React.FC = () => {
     fetchCompanyDropdown();
   }, []);
 
+  useEffect(() => {
+    const fetchLandingPageData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/cardRecords");
+        setGuaranteeData(response.data);
+      } catch (error) {
+        console.log("failed to fetch data");
+        setError((error as AxiosError).message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchLandingPageData();
+  }, []);
+
   const handleChange = (selectedCompany: string) => {
     console.log("selectedCompany", selectedCompany);
+  };
+
+  // Function to swap two users based on index
+  const handleClick = (index1: number, index2: number) => {
+    setGuaranteeData((prevGuarantees) => {
+      if (
+        index1 < 0 ||
+        index2 < 0 ||
+        index1 >= prevGuarantees.length ||
+        index2 >= prevGuarantees.length
+      ) {
+        return prevGuarantees; // Return unchanged if out of bounds
+      }
+
+      // Create a shallow copy of the array
+      const updatedGuarantees = [...prevGuarantees];
+
+      // Swap users at index1 and index2
+      [updatedGuarantees[index1], updatedGuarantees[index2]] = [
+        updatedGuarantees[index2],
+        updatedGuarantees[index1],
+      ];
+
+      updatedGuarantees[index1].status = "Active";
+      updatedGuarantees[index2].status = "Pending";
+      return updatedGuarantees;
+    });
   };
 
   return (
@@ -46,7 +90,7 @@ const Home: React.FC = () => {
 
       {/* Guarantee Cards */}
       <div className="container mt-4">
-        <GuaranteeCards />
+        <GuaranteeCards guaranteeData={guaranteeData} clickCard={handleClick} />
       </div>
 
       {/* Search Form */}
